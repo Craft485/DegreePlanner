3b81d6bd8a72839555f3644f345b4cee52a6127b
Colin Davis <colindavis5@gmail.com>
2026-03-30 20:30:15 -0400

    chore: cleanup logger.ts

--- services/server/src/utils/logger.ts
+++ services/server/src/utils/logger.ts
@@ -2,14 +2,12 @@ import { createLogger, format, transports } from "winston"
 
 const logger = createLogger({
   level: "debug",
-  // format: format.combine(format.colorize(), format.simple(), format.timestamp()),
   transports: [
     new transports.Console({
       level: "info",
       format: format.combine(
         format.colorize({ all: true }),
         format.timestamp(),
-        // format.prettyPrint(),
         format.printf(info => `${info.timestamp} [${info.level}] ${info.message}: ${info.method} ${info.url} returned ${info.statusCode} to ${info.ip}`)
       ),
     })
