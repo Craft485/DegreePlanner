dae2effe78df008553fca96a46e3a975e3a958bf
Colin Davis <colindavis5@gmail.com>
2026-06-27 17:34:30 -0400

    fix(client): add edge routing between coreqs

--- services/client/components/graph/edge.tsx
+++ services/client/components/graph/edge.tsx
@@ -26,57 +26,68 @@ export function CustomEdge({
 }: Readonly<EdgeProps>) {
   const { customEdgePaths, courseMap: map } = useContext(GraphContext)!
   const key = `${source}:${target}`
+  const sourceVertex = map.get(source)!
+  const targetVertex = map.get(target)!
   let path = customEdgePaths.get(key) ?? ""
 
   if (!path) {
     const yDirection = sourceY - targetY > 0 ? -1 : 1
 
-    const currentPosition = {
-      x: sourceX + (0.25 * NODE_WIDTH),
-      y: (targetX - sourceX) <= 0.5 * NODE_WIDTH ? targetY : sourceY + (0.75 * NODE_HEIGHT * yDirection),
-    } satisfies Position
-    const targetPosition = { x: targetX, y: targetY } as const satisfies Position
-    
-    const points: Position[] = []
-    const clampY = Math[sourceY - targetY > 0 ? "max" : "min"]
-    const minXDeltaToTarget = targetX - (0.25 * NODE_WIDTH)
-    
-    let proceedHorizontally = true
-    path = `
-      M${sourceX - 5},${sourceY}
-      L${currentPosition.x},${sourceY}
-      L${currentPosition.x},${currentPosition.y}`
-    
-    while (!positionIsWithinToleranceOfTarget(currentPosition, targetPosition)) {
-      const newPosition = { ...currentPosition } satisfies Position
+    if (sourceVertex.coReqs.includes(targetVertex.courseCode)) {
+      const xDirection = targetY - sourceY > 0 ? -1 : 1
+      path += `
+        M${sourceX - (xDirection === 1 ? 0 : NODE_WIDTH)},${sourceY}
+        l${xDirection * 0.25 * NODE_WIDTH},0
+        l0,${targetY - sourceY}
+        l${xDirection * -0.25 * NODE_WIDTH},0`
+    } else {
+      const currentPosition = {
+        x: sourceX + (0.25 * NODE_WIDTH),
+        y: (targetX - sourceX) <= 0.5 * NODE_WIDTH ? targetY : sourceY + (0.75 * NODE_HEIGHT * yDirection),
+      } satisfies Position
+      const targetPosition = { x: targetX, y: targetY } as const satisfies Position
       
-      // Determine direction to travel
-      if (proceedHorizontally) {
-        const newX = newPosition.x + (1.5 * NODE_WIDTH)
-        const clampedX = Math.min(newX, targetX)
-        if (currentPosition.x < minXDeltaToTarget || currentPosition.y === targetY) {
-          newPosition.x = clampedX
+      const points: Position[] = []
+      const clampY = Math[sourceY - targetY > 0 ? "max" : "min"]
+      const minXDeltaToTarget = targetX - (0.25 * NODE_WIDTH)
+      
+      let proceedHorizontally = true
+      path += `
+        M${sourceX - 5},${sourceY}
+        L${currentPosition.x},${sourceY}
+        L${currentPosition.x},${currentPosition.y}`
+      
+      while (!positionIsWithinToleranceOfTarget(currentPosition, targetPosition)) {
+        const newPosition = { ...currentPosition } satisfies Position
+        
+        // Determine direction to travel
+        if (proceedHorizontally) {
+          const newX = newPosition.x + (1.5 * NODE_WIDTH)
+          const clampedX = Math.min(newX, targetX)
+          if (currentPosition.x < minXDeltaToTarget || currentPosition.y === targetY) {
+            newPosition.x = clampedX
+          }
+        } else {
+          const newY = newPosition.y + (1.5 * NODE_HEIGHT * yDirection)
+          const clampedY = clampY(newY, targetY)
+          if (clampedY === newY || currentPosition.x >= minXDeltaToTarget) {
+            newPosition.y = clampedY
+          }
         }
-      } else {
-        const newY = newPosition.y + (1.5 * NODE_HEIGHT * yDirection)
-        const clampedY = clampY(newY, targetY)
-        if (clampedY === newY || currentPosition.x >= minXDeltaToTarget) {
-          newPosition.y = clampedY
+        
+        if (newPosition.x !== currentPosition.x || newPosition.y !== currentPosition.y) {
+          points.push({ ...newPosition })
+          currentPosition.x = newPosition.x
+          currentPosition.y = newPosition.y
         }
+        
+        proceedHorizontally = !proceedHorizontally
       }
       
-      if (newPosition.x !== currentPosition.x || newPosition.y !== currentPosition.y) {
-        points.push({ ...newPosition })
-        currentPosition.x = newPosition.x
-        currentPosition.y = newPosition.y
+      for (let i = 0; i < points.length; i++) {
+        const { x, y } = points[i]
+        path += `L${x},${y}`
       }
-
-      proceedHorizontally = !proceedHorizontally
-    }
-    
-    for (let i = 0; i < points.length; i++) {
-      const { x, y } = points[i]
-      path += `L${x},${y}`
     }
     
     customEdgePaths.set(key, path)
@@ -88,7 +99,7 @@ export function CustomEdge({
       path={customEdgePaths.get(key)!}
       style={{
         strokeWidth: "2",
-        stroke: `#${map.get(source)!.color}`,
+        stroke: `#${sourceVertex.color}`,
       }}
       markerEnd={markerEnd}
     />
